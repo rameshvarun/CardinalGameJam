afc3710780945d5c4686e7b1f1d5c6acc1602abf
Harrison Ho <harrison8989@gmail.com>
2014-11-01 15:21:57 -0700

    Add Ship Goodies

---
+++ Assets/ShipBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipBehavior : MonoBehaviour {
+
+	public boolean isPlayer;
+	public Vector3 color;
+	public int health;
+	public const int maxHealth = 100;
+
+	// Use this for initialization
+	void Start () {
+		health = maxHealth;
+
+		//retrieve data about the player
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
