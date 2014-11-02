106b1aa0bf59b2fa8034fbb8cd88cfc6bb6f8693
Varun Ramesh <varun.ramesh@aol.com>
2014-11-01 19:44:52 -0700

    Background scrolling

---
+++ Assets/ScrollScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollScript : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0, - speed * Time.deltaTime, 0);
+		if(transform.position.y < -15) {
+			Destroy(this.gameObject);
+		}
+	}
+}
